423268e62165bf3f738b8433364136f00b591078
fs814 <fs814@163.com>
2024-12-24 18:09:37 +0800

    fix window move

--- site-lisp/config/init-evil.el
+++ site-lisp/config/init-evil.el
@@ -4,6 +4,11 @@
 (lazy-load-global-keys
   '(
     ("C-g" . evil-force-normal-state)
+    ("C-c" . evil-force-normal-state)
+    ("C-l" . evil-window-right)
+    ("C-h" . evil-window-left)
+    ("C-j" . evil-window-down)
+    ("C-k" . evil-window-up)
     )
   "init-evil")
 
